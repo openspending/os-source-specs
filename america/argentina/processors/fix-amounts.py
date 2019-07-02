f8f662ce67e36d81871ac750f4c38c6a9c77afb1
Victor Nițu <victor@thesponge.eu>
2019-07-02 14:53:12 +0300

    Add missing processor for Argentina

---
+++ america/argentina/processors/fix-amounts.py
@@ -0,0 +1,22 @@
+#encoding: utf8
+from datetime import datetime
+from datapackage_pipelines.wrapper import ingest, spew
+
+params, datapackage, res_iter = ingest()
+
+columns = params.get('columns')
+
+def fix_amount(v):
+    v = v.replace(',', '.')
+    return float(v) * 1000000
+
+
+def process_resources(_res_iter):
+    for rows in _res_iter:
+        def process_rows(_rows):
+            for row in _rows:
+                yield dict((k, v if k not in columns else fix_amount(v)) for (k, v)
+                           in row.items())
+        yield process_rows(rows)
+
+spew(datapackage, process_resources(res_iter))
